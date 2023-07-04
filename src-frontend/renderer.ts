c9498c6aaaf5d2d51d204ceeef58791c7cc4978b
whatphilipcodes <gerdes.philip@gmail.com>
2023-07-04 23:43:33 +0200

    add: three, resources structure

--- src-frontend/renderer.ts
+++ src-frontend/renderer.ts
@@ -27,5 +27,42 @@
  */
 
 import './index.css';
+import * as THREE from 'three';
 
-console.log('👋 This message is being logged by "renderer.js", included via webpack');
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Function to handle window resizing
+function onWindowResize() {
+    // Update the renderer's size
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    // Optionally update the camera aspect ratio if you have a camera in your scene
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+}
+
+// Call the function when the window is resized
+window.addEventListener('resize', onWindowResize, false);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.z = 5;
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+
+    renderer.render(scene, camera);
+}
+
+animate();

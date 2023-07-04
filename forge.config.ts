5beded4007aed021fd6f4a71810080d617cea432
whatphilipcodes <gerdes.philip@gmail.com>
2023-07-04 22:04:42 +0200

    add: src-py, ch: src -> src-frontend

--- forge.config.ts
+++ forge.config.ts
@@ -1,8 +1,6 @@
 import type { ForgeConfig } from '@electron-forge/shared-types';
 import { MakerSquirrel } from '@electron-forge/maker-squirrel';
 import { MakerZIP } from '@electron-forge/maker-zip';
-import { MakerDeb } from '@electron-forge/maker-deb';
-import { MakerRpm } from '@electron-forge/maker-rpm';
 import { AutoUnpackNativesPlugin } from '@electron-forge/plugin-auto-unpack-natives';
 import { WebpackPlugin } from '@electron-forge/plugin-webpack';
 
@@ -39,11 +37,11 @@ const config: ForgeConfig = {
         config: rendererConfig,
         entryPoints: [
           {
-            html: './src/index.html',
-            js: './src/renderer.ts',
+            html: './src-frontend/index.html',
+            js: './src-frontend/renderer.ts',
             name: 'main_window',
             preload: {
-              js: './src/preload.ts',
+              js: './src-frontend/preload.ts',
             },
           },
         ],

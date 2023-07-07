331c08dd759d1c7deac642e2eb7ea7f5fc337661
whatphilipcodes <gerdes.philip@gmail.com>
2023-07-07 02:27:44 +0200

    rm: test step (no args)

--- build.bat
+++ build.bat
@@ -3,8 +3,5 @@ call .env\Scripts\activate.bat
 rem Build Python Backend
 pyinstaller entry.spec --noconfirm
 
-rem Test Python Backend
-dist\backend\backend.exe
-
 rem Build Electron Frontend
 yarn run make

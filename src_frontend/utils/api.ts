8ecc0d8c859578820e32ace8cd8a765c4f4d3f55
whatphilipcodes <gerdes.philip@gmail.com>
2023-07-11 15:46:04 +0200

    add: ipc protocoll

--- src_frontend/utils/api.ts
+++ src_frontend/utils/api.ts
@@ -1,17 +1,31 @@
-import axios from 'axios';
-// import { getConnectInfo } from './misc';
+import axios, { AxiosInstance } from 'axios';
 
-// create axios instance on startup with CORS enabled
-const api = axios.create({
-    baseURL: '127.0.0.1:3000',
-    timeout: 1000,
-    headers: {
-        'Access-Control-Allow-Origin': 'self',
-        'Content-Type': 'application/json',
+let api: AxiosInstance;
+function initAxios(url: URL): AxiosInstance {
+    return axios.create({
+        baseURL: url.hostname,
+        timeout: 1000,
+        headers: {
+            'Access-Control-Allow-Origin': 'self',
+            'Content-Type': 'application/json',
+        }
+    });
+}
+
+(window as any).electronAPI.onBackURL((_event: unknown, data: string) => {
+    // console.log('data received:', data);
+    try {
+        const url = new URL(data);
+        api = initAxios(url);
+        // Use the IP address as needed in the renderer process
+    } catch (error) {
+        console.error('Invalid URL:', error);
+        // Handle the invalid URL error
     }
 });
 
 const get = async (url: string) => {
+    if (!api) throw new Error('API not initialized');
     try {
         const response = await api.get(url);
         return response.data;

4e68f32e8b1fb8b6a57a33432a9009857a33d16b
whatphilipcodes <gerdes.philip@gmail.com>
2023-07-11 21:33:02 +0200

    rm: electron-shared-state package

---
+++ src_frontend/state/shared.ts
@@ -0,0 +1,9 @@
+import { interState } from "../utils/enums"
+
+const state = {
+    applicationState: interState,
+    elURL: URL,
+    uviURL: URL,
+}
+
+export default state
---
+++ src_frontend/utils/enums.ts
@@ -0,0 +1,9 @@
+enum interState {
+    loading = 'loading',
+    idle = 'idle',
+    interaction = 'interaction',
+    error = 'error',
+}
+
+// export
+export { interState };

b41d7734bac941080c0a8bd1efcd8a1c224fc318
whatphilipcodes <gerdes.philip@gmail.com>
2023-07-13 23:31:38 +0200

    fx: multiple three instances

--- webpack.renderer.config.ts
+++ webpack.renderer.config.ts
@@ -1,4 +1,5 @@
 import type { Configuration } from 'webpack'
+import path from 'path'
 
 import { rules } from './webpack.rules'
 import { plugins } from './webpack.plugins'
@@ -15,5 +16,8 @@ export const rendererConfig: Configuration = {
 	plugins,
 	resolve: {
 		extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
+		alias: {
+			three: path.resolve(__dirname, 'node_modules/three'),
+		},
 	},
 }
